60d7054fbfbf846e8faef40e2c0dc00d8ec8a938
coolAlias <coolAlias@users.noreply.github.com>
2013-09-27 07:46:37 +0900

    Added method to 'use' Sharingan
    Requires edits to PacketHandler, RegisterKeyBindings and NarutoKeyHandler to function.

--- NarutoStyleItems/Sharingan.java
+++ NarutoStyleItems/Sharingan.java
@@ -2,65 +2,62 @@ package NarutoStyleItems;
 
 import java.util.List;
 
-import org.lwjgl.input.Keyboard;
-
-import cpw.mods.fml.client.FMLClientHandler;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import NarutoStyle.NarutoStyle_main;
-import NarutoStyleEyeInventory.ExtendedPlayerProperties;
 import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
-public class Sharingan extends Item {
-
-	
+public class Sharingan extends Item
+{
 	public Sharingan(int par1) {
 		super(par1);
 		this.maxStackSize = 1;
 	}
-
 	
-	@Override
-	 public void onUpdate(ItemStack itemstack, World world, Entity entity, int par4, boolean isCurrentItem)
-	 {
-	 //EntityPlayer player = (EntityPlayer)entity;
-	 //ExtendedPlayerProperties props = (ExtendedPlayerProperties)player.getExtendedProperties(ExtendedPlayerProperties.EXT_PROP_NAME);
-
-	 // Choose whatever key you want
-	 /*if (FMLClientHandler.instance().getClient().inGameHasFocus && Keyboard.isKeyDown(Keyboard.KEY_M) && props != null) {
-         player.openGui(NarutoStyle_main.Instance, NarutoStyle_main.eyeGuiId, world, (int)player.posX, (int)player.posY, (int)player.posZ);
-	 }
-         
-         
-	 
-        	    
-         
- 
-	 if (Keyboard.isKeyDown(Keyboard.KEY_N) && props != null)
-     {
-      props.eyeInventory.nextActiveSlot();
-     }
-	 
-	 
-	 }*/
-	 }
-	
-	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean i)
+	/**
+	 * A method to call when active Sharingan is used with a key press
+	 * @param stack
+	 * @param player
+	 * @return true if Sharingan was activated successfully
+	 */
+	public static boolean useSharingan(ItemStack stack, EntityPlayer player)
 	{
-	list.add("Radius of 10 Blocks");
+		// Double-check whether this method was called with correct Item type
+		if (stack != null && stack.getItem() instanceof Sharingan)
+		{
+			if (!player.worldObj.isRemote) {
+				player.addChatMessage("Sharingan was used!");
+
+				List<EntityLivingBase> entl = player.worldObj.getEntitiesWithinAABB(EntityLivingBase.class, player.boundingBox.expand(10, 10, 10));
+				if (!entl.isEmpty()) {
+					for (EntityLivingBase el : entl) {
+						if (el != player) {
+							el.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id,100,2));
+						}
+					}
+				}
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
-	 @Override
-	 @SideOnly(Side.CLIENT)
-	 public void registerIcons(IconRegister iconRegister) {
-	          this.itemIcon = iconRegister.registerIcon("narutostyle:Sharingan");
-	 }
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean i)
+	{
+		list.add("Radius of 10 Blocks");
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister iconRegister) {
+		itemIcon = iconRegister.registerIcon(NarutoStyle_main.modid + ":" + getUnlocalizedName().substring(5).toLowerCase());
+	}
 }
