aa372bcbc4160a67fc4f73bbf7e68116373a93f1
coolAlias <coolAlias@users.noreply.github.com>
2013-09-26 22:51:30 +0900

    Added methods for ExtendedPlayer to use
    Some methods shouldn't be in here, such as onInit and registerEvents - these should be done in the main mod

--- NarutoStyle/CommonProxy.java
+++ NarutoStyle/CommonProxy.java
@@ -6,25 +6,32 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 
 public class CommonProxy  {
 
-	 public void registerRenderers() {
-     }
-	 public int addArmor(String string)
-	 {
+	/** Used to store IExtendedEntityProperties data temporarily between player death and respawn or dimension change */
+	private static final Map<String, NBTTagCompound> extendedEntityData = new HashMap<String, NBTTagCompound>();
+	
+	public void registerRenderers() {}
+	
+	// This should be done in the main mod as well
+	public void registerEvents()
+	 
+	public int addArmor(String string) {
 		 return 0;
-	 }
-	public void onInit(FMLInitializationEvent evt) {
-		
-		
+	}
+	
+	// What's this doing here? These kinds of methods belong in the main class with @EventHandler notation
+	public void onInit(FMLInitializationEvent evt) {}
+	
+	/**
+	 * Adds an entity's custom data to the map for temporary storage
+	 */
+	public static void storeEntityData(String name, NBTTagCompound compound) {
+		extendedEntityData.put(name, compound);
 	}
 
-    /*
-     *  Methods for ClientProxy to Override
-     */
-    public void registerEvents() {
-    	
-    	
-    }
-	
-
-	
+	/**
+	 * Removes the compound from the map and returns the NBT tag stored for name or null if none exists
+	 */
+	public static NBTTagCompound getEntityData(String name) {
+		return extendedEntityData.remove(name);
+	}
 }
