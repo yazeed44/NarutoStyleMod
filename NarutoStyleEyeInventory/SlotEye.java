8ee90f3802bea372c0e70c570b2756fe650f3572
coolAlias <coolAlias@users.noreply.github.com>
2013-09-26 21:50:03 +0900

    Fixed so can't take stack in active slot

--- NarutoStyleEyeInventory/SlotEye.java
+++ NarutoStyleEyeInventory/SlotEye.java
@@ -5,44 +5,42 @@ import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
-public class SlotEye extends Slot {
-	public SlotEye(IInventory inventory, int par2, int par3, int par4)
-	{
-	super(inventory, par2, par3, par4);
+public class SlotEye extends Slot
+{
+	public SlotEye(IInventory inventory, int par2, int par3, int par4) {
+		super(inventory, par2, par3, par4);
 	}
 
 	/**
-	         * Returns the maximum stack size for a given slot
-	         */
+	 * Returns the maximum stack size for a given slot
+	 */
 	@Override
-	public int getSlotStackLimit()
-	{
+	public int getSlotStackLimit() {
 	         return 1;
 	}
 
 	/**
-	* Check if the stack is a valid item for this slot. Always true beside for the armor slots.
-	*/
+	 * Check if the stack is a valid item for this slot. Always true beside for the armor slots.
+	 */
 	@Override
-	public boolean isItemValid(ItemStack itemstack)
-	{
-	return (itemstack.getItem() instanceof Sharingan ? true : false);
-	//return true;
+	public boolean isItemValid(ItemStack itemstack) {
+		return itemstack.getItem() instanceof Sharingan;
 	}
+}
 
-	}
-
-	class SlotEyeActive extends SlotEye
-	{
-	public SlotEyeActive(IInventory inventory, int par2, int par3, int par4)
-	{
-	super(inventory, par2, par3, par4);
+class SlotEyeActive extends SlotEye
+{
+	public SlotEyeActive(IInventory inventory, int par2, int par3, int par4) {
+		super(inventory, par2, par3, par4);
 	}
 
 	@Override
-	public boolean isItemValid(ItemStack itemstack)
-	{
-	return false;
+	public boolean isItemValid(ItemStack itemstack) {
+		return false;
 	}
-
+	
+	@Override
+	public boolean canTakeStack(EntityPlayer player) {
+		return false;
+    	}
 }
